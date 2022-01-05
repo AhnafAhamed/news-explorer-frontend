f14f3070be0bb0574de7886da694bfb8432d69d6
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-05 11:05:55 +0530

    feat: implement expanding and closing of mobile navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,21 @@ import Navigation from "../Navigation/Navigation";
 import './Header.css'
 
 function Header () {
+
+    function handleMenuClick() {
+        document.querySelector('.navigation').classList.add('navigation_mobile');
+        document.querySelector('.header').classList.add('header_mobile');
+    }
+
+    function handleCloseClick() {
+        document.querySelector('.navigation').classList.remove('navigation_mobile');
+        document.querySelector('.header').classList.remove('header_mobile');
+    }
+
     return (
-        <div className="header header_mobile">
+        <div className="header">
             <p className="header__logo">NewsExplorer</p>
-            <Navigation/>
+            <Navigation onMenuClick={handleMenuClick} onCloseClick={handleCloseClick}/>
         </div>
         
     )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,15 +2,15 @@ import './Navigation.css';
 import menu from '../../images/menu.svg';
 import close from '../../images/close.svg';
 
-function Navigation() {
+function Navigation({onMenuClick, onCloseClick}) {
     return (
-        <nav className="navigation navigation_mobile">
+        <nav className="navigation ">
             <p className="navigation__item  navigation__item_home navigation__item_active">Home</p>
             <p className="navigation__item navigation__item_article">Saved Articles</p>
             <p className="navigation__item navigation__item_signin">Sign in</p>
             <p className="navigation__item navigation__item_signout">Sign Out</p>
-            <img src={menu} alt="menu" className="navigation__item navigation__menu-mobile" />
-            <img src={close} alt="menu" className="navigation__close-mobile" />
+            <img src={menu} onClick={onMenuClick} alt="menu" className="navigation__item navigation__menu-mobile" />
+            <img src={close} onClick={onCloseClick} alt="close" className="navigation__close-mobile" />
         </nav>
     )
 }
