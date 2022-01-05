90df9600076fb8bce55b2b7593805f5a81661c04
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-05 11:15:16 +0530

    feat: create search results component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,13 @@ import "./Main.css";
 import Hero from "../Hero/Hero";
 import About from "../About/About";
 import Footer from "../Footer/Footer";
+import SearchResults from "../SearchResults/SearchResults";
 
 function Main() {
     return (
         <div className="main">
             <Hero/>
+            <SearchResults/>
             <About/>
             <Footer/>
         </div>
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import "./SearchResults.css";
+
+function SearchResults() {
+    return (
+        <div className="search-results">
+            <h3 className="search-results__heading">Search Results</h3>
+
+        </div>
+    )
+}
+
+export default SearchResults;
