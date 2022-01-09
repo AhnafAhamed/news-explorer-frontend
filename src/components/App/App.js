04fef50842950de06b6f39a766fb80d037a532ab
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-09 06:54:14 +0530

    fix: move components from main.js to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,21 @@
-import Main from '../Main/Main';
-import './App.css';
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+import Hero from "../Hero/Hero";
+import Main from "../Main/Main";
+import SavedNews from "../SavedNews/SavedNews";
+import SearchResults from "../SearchResults/SearchResults";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <Main>
+        <Hero />
+        <SavedNews />
+        <SearchResults />
+        <About />
+        <Footer />
+      </Main>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,5 @@
-import "./Main.css";
-import Hero from "../Hero/Hero";
-import About from "../About/About";
-import Footer from "../Footer/Footer";
-import SearchResults from "../SearchResults/SearchResults";
-import SavedNews from "../SavedNews/SavedNews";
-import PopupWithForm from "../PopupWithForm/PopupWithForm";
-
-function Main() {
-  return (
-    <div className="main">
-      <Hero />
-      <PopupWithForm
-        title="Sign in"
-        label="email"
-        inputType="email"
-        inputPlaceholder="email"
-        buttonText="Sign in"
-        redirectText="Sign up"
-      />
-      <SavedNews />
-      <SearchResults />
-      <About />
-      <Footer />
-    </div>
-  );
+function Main({ children }) {
+  return <div className="main">{children}</div>;
 }
 
 export default Main;
