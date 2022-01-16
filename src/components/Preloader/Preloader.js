06cd72cd82515767dfdadec50195132526455945
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-16 22:03:13 +0530

    feat: add preloader

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import "./Preloader.css";
+
+function Preloader() {
+    return(
+        <div className="preloader">
+            <i className="preloader__icon"></i>
+        </div>
+    )
+}
+
+export default Preloader;
