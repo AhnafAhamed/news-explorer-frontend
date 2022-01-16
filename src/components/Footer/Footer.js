85e96de6d2fa169b15272d97f2481816dc81b519
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-16 10:40:36 +0530

    fix: footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,49 @@
 import "./Footer.css";
 import fbIcon from "../../images/fb.svg";
 import githubIcon from "../../images/github.svg";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__copyrights">© 2021 Supersite, Powered by News API</p>
-            <div className="footer__navigation">
-                <div className="footer__links">
-                    <p className="footer__link">Home</p>
-                    <p className="footer__link">Practicum by Yandex</p>
-                </div>
-                <div className="footer__social-links">
-                    <img src={githubIcon} alt="" className="footer__social-link" />
-                    <img src={fbIcon} alt="" className="footer__social-link" />
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <p className="footer__copyrights">
+        © 2021 Supersite, Powered by News API
+      </p>
+      <div className="footer__navigation">
+        <div className="footer__links">
+          <NavLink to="/" className="footer__link">
+            Home
+          </NavLink>
+          <a
+            className="footer__link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://practicum.yandex.com/"
+          >
+            Practicum by Yandex
+          </a>
+        </div>
+        <div className="footer__social-links">
+          <a
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/AhnafAhamed/news-explorer-frontend"
+          >
+            <img src={githubIcon} alt="github" />
+          </a>
+          <a
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.facebook.com/YPracticum"
+          >
+            <img src={fbIcon} alt="facebook" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
