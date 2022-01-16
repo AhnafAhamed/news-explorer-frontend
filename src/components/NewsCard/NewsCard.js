0c052b84d3f7dc5b3c2765adab875070214b5795
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-16 09:37:21 +0530

    fix: show/hide news card icons based on route

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -2,16 +2,17 @@ import "./NewsCard.css";
 import image1 from "../../images/image_01.png";
 import deleteIcon from "../../images/delete.svg";
 import bookmarkIcon from "../../images/bookmark.svg";
+import { useLocation } from "react-router-dom";
 
 
 function NewsCard({isLoggedIn}) {
+    const route = useLocation();
     return (
         <div className="news-card">
             <div className="news-card__image-container">
                 <img src={image1} alt="" className="news-card__image" />
                 <p className="news-card__tag">Nature</p>
-                <img src={deleteIcon} alt="delete" className="news-card__icon news-card__icon_delete news-card__icon_hide" />
-                <img src={bookmarkIcon} alt="bookmark" className="news-card__icon news-card__icon_bookmark" />
+                { route.pathname === "/" ? <img src={bookmarkIcon} alt="bookmark" className="news-card__icon news-card__icon_bookmark" /> : <img src={deleteIcon} alt="delete" className="news-card__icon news-card__icon_delete" /> }
                 { !isLoggedIn ? <p className="news-card__action-prompt">Sign in to save articles</p> : ''}
             </div>
             <div className="news-card__text">
