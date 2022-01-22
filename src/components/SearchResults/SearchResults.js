104f907eee369c3c5025a31fc47d3f1b56ef2f19
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-22 21:45:14 +0530

    fix: remove unnecessary code

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,7 +2,6 @@ import NewsCardList from "../NewsCardList/NewsCardList";
 import "./SearchResults.css";
 
 function SearchResults({ isLoggedIn, newsCards }) {
-  console.log(typeof newsCards);
   return (
     <div className="search-results">
       <h3 className="search-results__heading">Search Results</h3>
