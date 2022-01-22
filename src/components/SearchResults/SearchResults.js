9004fcc6ff6b9da81b60fa4f86a608dd9a760802
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-22 13:31:55 +0530

    feat: implement nothing found block

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,13 +1,18 @@
 import NewsCardList from "../NewsCardList/NewsCardList";
 import "./SearchResults.css";
 
-function SearchResults( {isLoggedIn, newsCards, onLoadArticles}) {
-    return (
-        <div className="search-results">
-            <h3 className="search-results__heading">Search Results</h3>
-            <NewsCardList newsCards={newsCards} isLoggedIn={isLoggedIn}/>
-        </div>
-    )
+function SearchResults({ isLoggedIn, newsCards }) {
+  console.log(typeof newsCards);
+  return (
+    <div className="search-results">
+      <h3 className="search-results__heading">Search Results</h3>
+      {Object.keys(newsCards).length === 0 ? (
+        <p className="search-results__empty">Nothing found</p>
+      ) : (
+        <NewsCardList newsCards={newsCards} isLoggedIn={isLoggedIn} />
+      )}
+    </div>
+  );
 }
 
 export default SearchResults;
