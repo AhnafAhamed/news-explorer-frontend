ac2739da8f35c4e39e107c3a45d6d7de413bfb3f
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-22 22:51:16 +0530

    feat: newscard bookmark saved state and delete icon hover state

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -14,8 +14,9 @@ function NewsCard({isLoggedIn, image, source, date, title, description,category 
             <div className="news-card__image-container">
                 <img src={image} alt="" className="news-card__image" />
                 { route.pathname === "/saved-news" ? <p className="news-card__tag">{category}</p> : ""}
-                { route.pathname === "/" ? <button className="news-card__icon news-card__icon_bookmark"></button> : <button  className="news-card__icon news-card__icon_delete"></button> }
-                { !isLoggedIn ? <p className="news-card__action-prompt">Sign in to save articles</p> : ''}
+                { route.pathname === "/" ? <button className="news-card__icon news-card__icon-bookmark news-card__icon-bookmark_saved"></button> : <button  className="news-card__icon news-card__icon-delete"></button> }
+                { !isLoggedIn ? <p className="news-card__save-prompt">Sign in to save articles</p> : ''}
+                { route.pathname === "/" ? <p className="news-card__delete-prompt">Remove from saved</p> : ''}
             </div>
             <div className="news-card__text">
                 <p className="news-card__date">{parseDate(date).toLocaleDateString('default', { year: 'numeric', day: 'numeric', month: 'long' })}</p>
