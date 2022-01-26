aa7992ec5a2cec5018c5173b1c518e4bf569b6a8
AhnafAhamed <ahnafahamed4@gmail.com>
2022-01-26 23:20:41 +0530

    fix: not found block

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,4 +1,5 @@
 import NewsCardList from "../NewsCardList/NewsCardList";
+import NotFound from "../NotFound/NotFound";
 import "./SearchResults.css";
 
 function SearchResults({ isLoggedIn, newsCards }) {
@@ -6,7 +7,7 @@ function SearchResults({ isLoggedIn, newsCards }) {
     <div className="search-results">
       { Object.keys(newsCards).length === 0 ? '' : <h3 className="search-results__heading">Search Results</h3>}
       {Object.keys(newsCards).length === 0 ? (
-        <p className="search-results__empty">Nothing found</p>
+        <NotFound/>
       ) : (
         <NewsCardList newsCards={newsCards} isLoggedIn={isLoggedIn} />
       )}
