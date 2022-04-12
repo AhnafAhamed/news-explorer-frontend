ac3a911dcfba813ade1f6f498fa972bcfd11fd13
AhnafAhamed <ahnafahamed4@gmail.com>
2022-04-13 00:04:16 +0530

    feat: add article count to savedNewsPage

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,13 +1,25 @@
 import "./SavedNewsHeader.css";
 
 function SavedNewsHeader({ newsCards }) {
+  function categories () {
+    let categoryText = "";
+    const categoriesList = newsCards.map((item) => {
+      return item.keyword
+    })
+    if(categoriesList.length > 2) {
+      categoryText = categoriesList.slice(0,2).join(', ') + ' and ' + categoriesList.slice(2).length + ' others'
+    } else {
+      categoryText = categoriesList.join(', ')
+    }
+    return categoryText
+  }
   return (
     <div className="saved-news-header">
       <p className="saved-news-header__title">Saved articles</p>
       <h3 className="saved-news-header__info">
         {newsCards.length === 0
           ? "You dont have any articles saved"
-          : "Elise, you have 5 saved articles"}
+          : `Elise, you have ${newsCards.length} saved articles`}
       </h3>
       {newsCards.length === 0 ? (
         ""
@@ -17,7 +29,8 @@ function SavedNewsHeader({ newsCards }) {
             By Keywords:{" "}
           </span>
           <span className="saved-news-header__description_keywords">
-            Nature, Wild and 2 other
+            {/* Nature, Wild and 2 other */}
+            {categories()}
           </span>
         </div>
       )}
