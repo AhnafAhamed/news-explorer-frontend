8cc341411bc90c8d7ede42807b1f2aec0b4ad1ff
AhnafAhamed <ahnafahamed4@gmail.com>
2022-12-29 14:21:53 +0530

    refactor: MainApi

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,7 +1,6 @@
 class MainApi {
   constructor(params) {
     this.baseUrl = params.baseUrl;
-    this.headers = params.headers;
   }
 
   _checkResponse(res) {
@@ -11,17 +10,26 @@ class MainApi {
     return Promise.reject("Error");
   }
 
+  _headers() {
+    const token = localStorage.getItem("token");
+    return {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+  }
+
   getArticles() {
     return fetch(this.baseUrl + "/articles", {
       method: "GET",
-      headers: this.headers,
+      headers: this._headers(),
     }).then((res) => this._checkResponse(res));
   }
 
   saveArticle({ keyword, title, text, date, source, link, image }) {
     return fetch(this.baseUrl + "/articles", {
       method: "POST",
-      headers: this.headers,
+      headers: this._headers(),
       body: JSON.stringify({
         keyword,
         title,
@@ -37,20 +45,14 @@ class MainApi {
   deleteArticle(articleId) {
     return fetch(this.baseUrl + `/articles/${articleId}`, {
       method: "DELETE",
-      headers: this.headers,
-    }).then((res) => this._checkResponse(res))
-
+      headers: this._headers(),
+    }).then((res) => this._checkResponse(res));
   }
 }
 
 const mainApi = new MainApi({
   // baseUrl: "https://api.explore.students.nomoreparties.sbs",
   baseUrl: "http://localhost:3000",
-  headers: {
-    'Accept': 'application/json',
-    "Authorization": `Bearer ${localStorage.getItem("token")}`,
-    "Content-Type": "application/json",
-  },
 });
 
 export default mainApi;
