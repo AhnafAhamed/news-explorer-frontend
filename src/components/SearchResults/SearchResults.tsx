cf9aa7d203248a863a6e5150cc189a297ef568a6
AhnafAhamed <ahnafahamed4@gmail.com>
2023-01-15 17:27:12 +0530

    feat: SearchResults to TS

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -2,7 +2,13 @@ import NewsCardList from "../NewsCardList/NewsCardList";
 import NotFound from "../NotFound/NotFound";
 import "./SearchResults.css";
 
-function SearchResults({ isLoggedIn, newsCards, keyword }) {
+type SearchResultsProps = {
+  isLoggedIn: boolean;
+  newsCards: Array<any>;
+  keyword: string;
+};
+
+function SearchResults({ isLoggedIn, newsCards, keyword }: SearchResultsProps) {
   return (
     <div className="search-results">
       { Object.keys(newsCards).length === 0 ? '' : <h3 className="search-results__heading">Search Results</h3>}
